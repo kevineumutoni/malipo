029912910a54c2d352de803412f3ad7d5dd07830
magretjumbedavid <magretjumbedavid@gmail.com>
2025-09-11 10:28:54 +0300

    updated pull request

--- api/views.py
+++ api/views.py
@@ -16,11 +16,4 @@ class PolicyViewSet(viewsets.ModelViewSet):
     queryset = Policy.objects.all()
     serializer_class = PolicySerializer
 
-    def create(self, request, *args, **kwargs):
-        print("Request data:", request.data)
-        serializer = self.get_serializer(data=request.data)
-        if not serializer.is_valid():
-            print("Errors:", serializer.errors)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        self.perform_create(serializer)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+   
