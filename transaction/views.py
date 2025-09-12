89513c6fc1dffd167625ac5f75fd9e92290dcd3a
student <oumagladwineesther@gmail.com>
2025-09-12 10:32:36 +0300

    Made migrations

--- transaction/views.py
+++ transaction/views.py
@@ -9,8 +9,6 @@ from .daraja import DarajaAPI
 from .serializers import STKPushSerializer
 from .models import Transaction  
 from django.conf import settings
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
 from .models import Transaction
 
 
