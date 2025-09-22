e3738b7d31fea73338d2e68f30ed0cd35af64d7a
student <oumagladwineesther@gmail.com>
2025-09-22 21:53:57 +0300

    fixed the endpoints

--- malipoflex/settings.py
+++ malipoflex/settings.py
@@ -29,7 +29,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = ["*"]
 # Application definition
 INSTALLED_APPS = [
